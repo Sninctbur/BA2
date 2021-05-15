c0119169ed2bc996681017ff9c97dc7bfb1080b5
GammaWhiskey <speedmgaming@gmail.com>
2021-05-14 22:07:40 -0400

    Contaminant Cloud Remover tool!

---
+++ lua/weapons/gmod_tool/stools/ba2_tool_virus_cloud.lua
@@ -0,0 +1,19 @@
+TOOL.Category = "Bio-Annihilation II"
+TOOL.Name = "#tool.ba2_tool_virus_cloud.name"
+
+function TOOL:LeftClick( trace )
+    if SERVER and trace.Hit then
+        for i,ent in pairs(ents.FindInSphere(trace.HitPos,300)) do
+            if ent:GetClass() == "ba2_virus_cloud" then
+                ent:Remove()
+                return true  
+            end
+        end
+    end
+end
+
+if CLIENT then
+    language.Add("tool.ba2_tool_virus_cloud.name", "Contaminant Cloud Remover")
+    language.Add("tool.ba2_tool_virus_cloud.desc", "Clean up your clouds of zombifying gas with this dandy tool!")
+    language.Add("tool.ba2_tool_virus_cloud.0", "Left click to remove. Right click to spawn the Bio-Virus somewhere in the continental United States.")
+end
